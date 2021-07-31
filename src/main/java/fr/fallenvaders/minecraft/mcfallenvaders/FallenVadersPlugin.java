35749f428c63f8c7a83b5b4d9ca902157716b104
Voltariuss <loic.duboistermoz@gmail.com>
2021-07-31 15:30:48 +0200

    feat: creates plugin main class

---
+++ src/main/java/fr/fallenvaders/minecraft/mcfallenvaders/FallenVadersPlugin.java
@@ -0,0 +1,12 @@
+package fr.fallenvaders.minecraft.mcfallenvaders;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FallenVadersPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable()
+    {
+        System.out.println("TEST");
+    }
+}
