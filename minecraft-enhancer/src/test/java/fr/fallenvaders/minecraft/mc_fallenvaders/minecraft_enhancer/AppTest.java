fabd275b4d385e43a043e87a63645ca53dd6e36b
Voltariuss <loic.duboistermoz@gmail.com>
2021-07-31 20:07:37 +0200

    build: setups mùulti-module project with minecraft-enhancer as submodule

---
+++ minecraft-enhancer/src/test/java/fr/fallenvaders/minecraft/mc_fallenvaders/minecraft_enhancer/AppTest.java
@@ -0,0 +1,20 @@
+package fr.fallenvaders.minecraft.mc_fallenvaders.minecraft_enhancer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
