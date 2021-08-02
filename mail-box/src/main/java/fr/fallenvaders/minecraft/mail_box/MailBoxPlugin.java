885dfac55fcccca627afbd8a85a6072c6a528862
Voltariuss <loic.duboistermoz@gmail.com>
2021-08-02 21:00:16 +0200

    build: setups mail-box module

---
+++ mail-box/src/main/java/fr/fallenvaders/minecraft/mail_box/MailBoxPlugin.java
@@ -0,0 +1,12 @@
+package fr.fallenvaders.minecraft.mail_box;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MailBoxPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable()
+    {
+        System.out.println("TEST");
+    }
+}
