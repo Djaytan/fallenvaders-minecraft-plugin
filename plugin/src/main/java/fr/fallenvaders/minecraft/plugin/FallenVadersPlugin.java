6d509a9745ee068a6dcaa0cf6eda96aed598b048
Voltariuss <loic.duboistermoz@gmail.com>
2021-08-14 16:44:26 +0200

    feat: FV-46 - setups module initialization

--- plugin/src/main/java/fr/fallenvaders/minecraft/plugin/FallenVadersPlugin.java
+++ plugin/src/main/java/fr/fallenvaders/minecraft/plugin/FallenVadersPlugin.java
@@ -1,15 +1,21 @@
 package fr.fallenvaders.minecraft.plugin;
 
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import fr.fallenvaders.minecraft.mc_enhancer.controller.commands.PingCmd;
+import fr.fallenvaders.minecraft.plugin.modules.CompleteModuleRegisterInitializer;
+import fr.fallenvaders.minecraft.plugin.modules.ModuleRegister;
+import fr.fallenvaders.minecraft.plugin.modules.ModuleRegisterException;
+import fr.fallenvaders.minecraft.plugin.modules.ModuleRegisterInitializer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FallenVadersPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.getCommand("ping").setExecutor(new PingCmd());
-        System.out.println("FallenVadersPlugin: TEST");
+        try {
+            ModuleRegisterInitializer moduleInitializer = new CompleteModuleRegisterInitializer();
+            ModuleRegister moduleRegister = moduleInitializer.initialize();
+            System.out.println("FallenVaders plugin enabled.");
+        } catch (ModuleRegisterException e) {
+            e.printStackTrace();
+        }
     }
 }
