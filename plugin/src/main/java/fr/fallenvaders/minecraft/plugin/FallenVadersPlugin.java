da9fd2e99f602e6de7502cd9fc98b473a1802ca6
Voltariuss <loic.duboistermoz@gmail.com>
2021-08-14 16:42:21 +0200

    feat: FV-46 - removes ProtocolLib dependency

--- plugin/src/main/java/fr/fallenvaders/minecraft/plugin/FallenVadersPlugin.java
+++ plugin/src/main/java/fr/fallenvaders/minecraft/plugin/FallenVadersPlugin.java
@@ -7,16 +7,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class FallenVadersPlugin extends JavaPlugin {
 
-    private ProtocolManager protocolManager;
-
     @Override
     public void onEnable() {
-        this.protocolManager = ProtocolLibrary.getProtocolManager();
         this.getCommand("ping").setExecutor(new PingCmd());
         System.out.println("FallenVadersPlugin: TEST");
-    }
-
-    public ProtocolManager getProtocolManager() {
-        return this.protocolManager;
     }
 }
