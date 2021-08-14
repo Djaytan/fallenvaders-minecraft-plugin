560158ba0655254bc1d1f4fc0f730ee99ef15484
Voltariuss <loic.duboistermoz@gmail.com>
2021-08-14 15:45:25 +0200

    feat: FV-46 - declares minecraft-enhancer module

--- plugin/src/main/java/fr/fallenvaders/minecraft/plugin/modules/MinecraftEnhanceModuleRegister.java
+++ plugin/src/main/java/fr/fallenvaders/minecraft/plugin/modules/MinecraftEnhanceModuleRegister.java
@@ -1,10 +1,18 @@
 package fr.fallenvaders.minecraft.plugin.modules;
 
+import fr.fallenvaders.minecraft.mc_enhancer.controller.commands.PingCmd;
+
 public class MinecraftEnhanceModuleRegister extends ModuleRegister {
 
     public static final String MODULE_NAME = "minecraft-enhancer";
 
     public MinecraftEnhanceModuleRegister() {
         super(MODULE_NAME);
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("Module: TEST");
+        getPlugin().getCommand("ping").setExecutor(new PingCmd());
     }
 }
