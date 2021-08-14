4ffca774335c195fc8597b5000cc300477836c80
Voltariuss <loic.duboistermoz@gmail.com>
2021-08-14 14:43:02 +0200

    build: FV-46 - actives minecraft-enhancer module

--- plugin/src/main/java/fr/fallenvaders/minecraft/plugin/FallenVadersPlugin.java
+++ plugin/src/main/java/fr/fallenvaders/minecraft/plugin/FallenVadersPlugin.java
@@ -1,11 +1,22 @@
 package fr.fallenvaders.minecraft.plugin;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import fr.fallenvaders.minecraft.mc_enhancer.controller.commands.PingCmd;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FallenVadersPlugin extends JavaPlugin {
 
+    private ProtocolManager protocolManager;
+
     @Override
     public void onEnable() {
+        this.protocolManager = ProtocolLibrary.getProtocolManager();
+        this.getCommand("ping").setExecutor(new PingCmd());
         System.out.println("FallenVadersPlugin: TEST");
+    }
+
+    public ProtocolManager getProtocolManager() {
+        return this.protocolManager;
     }
 }
