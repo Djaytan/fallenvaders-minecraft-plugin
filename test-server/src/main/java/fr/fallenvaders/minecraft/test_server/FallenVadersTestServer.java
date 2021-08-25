e9ff32056650e7854bddcb150d1e5bd933b522bb
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-08-25 22:57:46 +0200

    build: FV-48 - adds java program in charge of running paper server

--- test-server/src/main/java/fr/fallenvaders/minecraft/test_server/FallenVadersTestServer.java
+++ test-server/src/main/java/fr/fallenvaders/minecraft/test_server/FallenVadersTestServer.java
@@ -1,8 +1,43 @@
 package fr.fallenvaders.minecraft.test_server;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
 public class FallenVadersTestServer {
 
-    public static void main(String[] args) {
+    public static final Logger LOGGER = Logger.getAnonymousLogger();
 
+    public static final String SERVER_JAR_NAME = "papermc-server.jar";
+
+    public static final String[] SERVER_LAUNCH_COMMAND = new String[]{
+        "java",
+        "-server",
+        "-Xms512M",
+        "-Xmx2G",
+        "-jar",
+        SERVER_JAR_NAME,
+        "nogui"
+    };
+
+    public static void main(String[] args) {
+        ProcessBuilder pb = new ProcessBuilder(SERVER_LAUNCH_COMMAND);
+        pb.directory(new File("server/"));
+        pb.inheritIO();
+        try {
+            LOGGER.info("Launching test server...");
+            LOGGER.info(String.join(" ", pb.command()));
+            Process p = pb.start();
+            try {
+                p.waitFor();
+            } catch (InterruptedException e) {
+                LOGGER.severe("Server interrupted!");
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            LOGGER.severe("Failed to launch test server! Maybe wrong default working directory setup?");
+            e.printStackTrace();
+        }
     }
 }
