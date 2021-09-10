7b2986bb497a1046ff28c4b05a31292f4ab1cd23
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-09-11 01:04:19 +0200

    doc: FV-38 - add Javadoc

--- plugin/src/main/java/fr/fallenvaders/minecraft/plugin/guice/FallenVadersModule.java
+++ plugin/src/main/java/fr/fallenvaders/minecraft/plugin/guice/FallenVadersModule.java
@@ -4,6 +4,12 @@ import com.google.inject.AbstractModule;
 import fr.fallenvaders.minecraft.plugin.FallenVadersPlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Guice module of the project.
+ *
+ * @author Voltariuss
+ * @since 0.2.0
+ */
 public class FallenVadersModule extends AbstractModule {
 
   @Override
