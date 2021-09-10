0c49e4494309a660075843a6b0f83ef08864946f
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-09-11 00:52:14 +0200

    feat: FV-38 - add injector class to apply dependency injection pattern

---
+++ plugin/src/main/java/fr/fallenvaders/minecraft/plugin/guice/FallenVadersInjector.java
@@ -0,0 +1,27 @@
+package fr.fallenvaders.minecraft.plugin.guice;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import fr.fallenvaders.minecraft.plugin.FallenVadersPlugin;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Applies Dependency Injection pattern through Guice library.
+ *
+ * @author Voltariuss
+ * @since 0.2.0
+ */
+public class FallenVadersInjector {
+
+  /**
+   * Injects dependencies through Guice.
+   *
+   * @param plugin The {@link FallenVadersPlugin} instance.
+   */
+  public void inject(@NotNull FallenVadersPlugin plugin) {
+    Module module = new FallenVadersModule();
+    Injector injector = Guice.createInjector(module);
+    injector.injectMembers(plugin);
+  }
+}
