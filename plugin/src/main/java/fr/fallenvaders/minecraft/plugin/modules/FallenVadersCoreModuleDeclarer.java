c49c3d7c32716f7971ea2f08f2f584118b066a73
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-09-14 13:54:45 +0200

    chore: FV-38 - add TODO

--- plugin/src/main/java/fr/fallenvaders/minecraft/plugin/modules/FallenVadersCoreModuleDeclarer.java
+++ plugin/src/main/java/fr/fallenvaders/minecraft/plugin/modules/FallenVadersCoreModuleDeclarer.java
@@ -2,6 +2,7 @@ package fr.fallenvaders.minecraft.plugin.modules;
 
 import fr.fallenvaders.minecraft.core.FallenVadersCore;
 
+// TODO: rename to FVCoreModuleDeclarer and move to sub-package (like others modules)
 public class FallenVadersCoreModuleDeclarer extends ModuleDeclarer {
 
   public static final String MODULE_NAME = "fallenvaders-core";
