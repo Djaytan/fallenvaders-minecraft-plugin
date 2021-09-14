c7e39437a546c76a147a0c5d4add36d151fe62b9
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-09-14 23:20:52 +0200

    style: FV-38 - add a first empty line in the class definition content

--- plugin/src/main/java/fr/fallenvaders/minecraft/plugin/modules/ModuleRegisterInitializer.java
+++ plugin/src/main/java/fr/fallenvaders/minecraft/plugin/modules/ModuleRegisterInitializer.java
@@ -7,6 +7,6 @@ import javax.inject.Singleton;
 @Singleton
 @ImplementedBy(CompleteModuleRegisterInitializer.class)
 public interface ModuleRegisterInitializer {
-  
+
   ModuleRegister initialize() throws ModuleRegisterException;
 }
