e641c8ebd9ea7af1d43590028322adc2d1a3f2af
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-09-14 23:31:58 +0200

    feat: FV-38 - finalize Guice setup of plugin entry class

--- plugin/src/main/java/fr/fallenvaders/minecraft/plugin/FallenVadersPlugin.java
+++ plugin/src/main/java/fr/fallenvaders/minecraft/plugin/FallenVadersPlugin.java
@@ -1,19 +1,19 @@
 package fr.fallenvaders.minecraft.plugin;
 
 import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import fr.fallenvaders.minecraft.plugin.guice.FallenVadersInjector;
-import fr.fallenvaders.minecraft.plugin.modules.CompleteModuleRegisterInitializer;
 import fr.fallenvaders.minecraft.plugin.modules.ModuleRegister;
 import fr.fallenvaders.minecraft.plugin.modules.ModuleRegisterException;
 import fr.fallenvaders.minecraft.plugin.modules.ModuleRegisterInitializer;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.inject.Singleton;
 import java.util.logging.Level;
 
 @Singleton
 public class FallenVadersPlugin extends JavaPlugin {
 
+  @Inject private ModuleRegisterInitializer moduleRegisterInitializer;
   @Inject private ModuleRegister moduleRegister;
 
   @Override
@@ -22,13 +22,14 @@ public class FallenVadersPlugin extends JavaPlugin {
     FallenVadersInjector fvInjector = new FallenVadersInjector();
     fvInjector.inject(this);
 
+    // Prepare config
     this.saveDefaultConfig();
 
+    // Initializes modules
     try {
-      ModuleRegisterInitializer moduleInitializer = new CompleteModuleRegisterInitializer();
-      moduleInitializer.initialize();
+      moduleRegisterInitializer.initialize();
       moduleRegister.enableModules();
-      getLogger().info("FallenVaders plugin enabled.");
+      getLogger().info("FallenVaders plugin successfully enabled.");
     } catch (ModuleRegisterException e) {
       // TODO: better error management
       getLogger().log(Level.SEVERE, "An error has occurred during modules registration.", e);
