8c48853cadd252ad61d77f9dd641f7ecc37e294b
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-09-16 21:35:21 +0200

    fix: FV-38 - add missing Mockito extension declaration

--- plugin/src/test/java/fr/fallenvaders/minecraft/plugin/modules/ModuleRegisterContainerTest.java
+++ plugin/src/test/java/fr/fallenvaders/minecraft/plugin/modules/ModuleRegisterContainerTest.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.inject.Inject;
 
+@ExtendWith(MockitoExtension.class)
 class ModuleRegisterContainerTest {
 
   @Mock private JavaPlugin javaPlugin;
