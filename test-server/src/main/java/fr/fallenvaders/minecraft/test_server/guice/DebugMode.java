d1f3e1db4cda8a8681ff3a3c737b134be54a8db1
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-09-17 23:27:09 +0200

    feat: FV-87 - setup Guice test server module

---
+++ test-server/src/main/java/fr/fallenvaders/minecraft/test_server/guice/DebugMode.java
@@ -0,0 +1,29 @@
+/*
+ *  This file is part of the FallenVaders distribution (https://github.com/FallenVaders).
+ *  Copyright © 2021 Loïc DUBOIS-TERMOZ.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fr.fallenvaders.minecraft.test_server.guice;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DebugMode {}
---
+++ test-server/src/main/java/fr/fallenvaders/minecraft/test_server/guice/TestServerModule.java
@@ -0,0 +1,57 @@
+/*
+ *  This file is part of the FallenVaders distribution (https://github.com/FallenVaders).
+ *  Copyright © 2021 Loïc DUBOIS-TERMOZ.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fr.fallenvaders.minecraft.test_server.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.BindingAnnotation;
+import com.google.inject.Provides;
+import fr.fallenvaders.minecraft.test_server.PropertiesUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Guice module for test server program.
+ *
+ * @author Voltariuss
+ * @since 0.2.0
+ */
+public final class TestServerModule extends AbstractModule {
+
+  private static final String CONFIG_FILE = "config.properties";
+
+  private final boolean debugMode;
+
+  public TestServerModule(boolean debugMode) {
+    this.debugMode = debugMode;
+  }
+
+  @Provides
+  @DebugMode
+  public boolean provideDebugMode() {
+    return debugMode;
+  }
+
+  @Provides
+  @ConfigProperties
+  @NotNull
+  public Properties provideConfigProperties() throws IOException {
+    return PropertiesUtils.getProperties(CONFIG_FILE);
+  }
+}
