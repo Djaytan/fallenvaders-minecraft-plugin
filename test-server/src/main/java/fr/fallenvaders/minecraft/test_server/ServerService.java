4bab0eadca85ed1c204697b2d78b533cdc97105f
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-09-25 23:22:27 +0200

    refactor: FV-87 - remove no longer used dependency

--- test-server/src/main/java/fr/fallenvaders/minecraft/test_server/ServerService.java
+++ test-server/src/main/java/fr/fallenvaders/minecraft/test_server/ServerService.java
@@ -18,14 +18,12 @@
 package fr.fallenvaders.minecraft.test_server;
 
 import fr.fallenvaders.minecraft.test_server.command.CommandExecutor;
-import fr.fallenvaders.minecraft.test_server.deploy.FVPluginJarNameAssembler;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.nio.file.Path;
 import java.util.Objects;
 
 /**
@@ -40,33 +38,25 @@ public final class ServerService {
   private static final Logger logger = LoggerFactory.getLogger(ServerService.class);
 
   private final CommandExecutor commandExecutor;
-  private final FVPluginJarNameAssembler fvPluginJarNameAssembler;
   private final ProgramPropertiesRegister programPropertiesRegister;
 
   /**
    * Constructor.
    *
    * @param commandExecutor The command executor.
-   * @param fvPluginJarNameAssembler The FV's plugin jar name assembler.
    * @param programPropertiesRegister The {@link ProgramProperties}'s register.
    */
   @Inject
   public ServerService(
       @NotNull CommandExecutor commandExecutor,
-      @NotNull FVPluginJarNameAssembler fvPluginJarNameAssembler,
       @NotNull ProgramPropertiesRegister programPropertiesRegister) {
     Objects.requireNonNull(commandExecutor);
-    Objects.requireNonNull(fvPluginJarNameAssembler);
     Objects.requireNonNull(programPropertiesRegister);
     this.commandExecutor = commandExecutor;
-    this.fvPluginJarNameAssembler = fvPluginJarNameAssembler;
     this.programPropertiesRegister = programPropertiesRegister;
   }
 
-  public void prepareServer() {
-    ProgramProperties programProperties = programPropertiesRegister.getProgramProperties();
-
-  }
+  public void prepareServer() {}
 
   /**
    * Starts the server according to the config properties. If an exception is thrown during the
