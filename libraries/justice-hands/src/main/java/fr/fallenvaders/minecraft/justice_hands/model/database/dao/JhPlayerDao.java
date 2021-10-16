93fa4a8beb744d16a8e04ad07f466f0314485f17
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-10-16 12:44:36 +0200

    feat: FV-104 - add JhPlayer DAO class and implements get methods

---
+++ libraries/justice-hands/src/main/java/fr/fallenvaders/minecraft/justice_hands/model/database/dao/JhPlayerDao.java
@@ -0,0 +1,108 @@
+/*
+ *  This file is part of the FallenVaders distribution (https://github.com/FallenVaders).
+ *  Copyright © 2021 Loïc DUBOIS-TERMOZ.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fr.fallenvaders.minecraft.justice_hands.model.database.dao;
+
+import fr.fallenvaders.minecraft.commons.sql.FvDao;
+import fr.fallenvaders.minecraft.commons.sql.FvDataSource;
+import fr.fallenvaders.minecraft.justice_hands.model.database.entities.JhPlayer;
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * DAO class of the {@link JhPlayer} entity class.
+ *
+ * <p>For more information about DAO pattern, see {@link FvDao} interface Javadoc.
+ *
+ * @author FallenVaders' dev team
+ * @since 0.3.0
+ * @see FvDao
+ */
+@Singleton
+public class JhPlayerDao implements FvDao<JhPlayer> {
+
+  // TODO: FV-116, FV-117 - optimisation with cache
+
+  private final FvDataSource fvDataSource;
+
+  /**
+   * Constructor.
+   *
+   * @param fvDataSource The FallenVaders' data source.
+   */
+  @Inject
+  public JhPlayerDao(@NotNull FvDataSource fvDataSource) {
+    this.fvDataSource = fvDataSource;
+  }
+
+  @Override
+  public Optional<JhPlayer> get(String id) throws SQLException {
+    UUID uuid = UUID.fromString(id);
+    try (Connection connection = fvDataSource.getConnection();
+        PreparedStatement stmt =
+            connection.prepareStatement("SELECT points FROM players_points WHERE uuid = ?")) {
+      stmt.setString(1, uuid.toString());
+      ResultSet rs = stmt.executeQuery();
+      JhPlayer jhPlayer = null;
+      if (rs.first()) {
+        jhPlayer = new JhPlayer(uuid, rs.getInt("points"));
+      }
+      return Optional.ofNullable(jhPlayer);
+    }
+  }
+
+  @Override
+  public List<JhPlayer> getAll() throws SQLException {
+    try (Connection connection = fvDataSource.getConnection();
+        PreparedStatement stmt =
+            connection.prepareStatement("SELECT uuid, points FROM players_points")) {
+      ResultSet rs = stmt.executeQuery();
+      List<JhPlayer> jhPlayers = new ArrayList<>(rs.getFetchSize());
+      while (rs.next()) {
+        UUID uuid = UUID.fromString(rs.getString("uuid"));
+        JhPlayer jhPlayer = new JhPlayer(uuid, rs.getInt("points"));
+        jhPlayers.add(jhPlayer);
+      }
+      return jhPlayers;
+    }
+  }
+
+  @Override
+  public void save(JhPlayer jhPlayer) throws SQLException {
+    throw new RuntimeException("Not implemented yet.");
+  }
+
+  @Override
+  public void update(JhPlayer jhPlayer) throws SQLException {
+    throw new RuntimeException("Not implemented yet.");
+  }
+
+  @Override
+  public void delete(JhPlayer jhPlayer) throws SQLException {
+    throw new RuntimeException("Not implemented yet.");
+  }
+}
