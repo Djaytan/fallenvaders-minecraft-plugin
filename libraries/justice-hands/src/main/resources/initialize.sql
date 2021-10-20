37df7eca5d202f2aba97e08450e241b626a97d30
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-10-20 17:39:38 +0200

    feat: FV-104 - update fv_jh_sanction SQL table colomns name

--- libraries/justice-hands/src/main/resources/initialize.sql
+++ libraries/justice-hands/src/main/resources/initialize.sql
@@ -6,14 +6,14 @@ CREATE TABLE IF NOT EXISTS `fv_jh_player`
 
 CREATE TABLE IF NOT EXISTS `fv_jh_sanction`
 (
-  `id`          INT(255) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `uuid`        CHAR(255)         NOT NULL,
-  `name`        CHAR(255)         NOT NULL,
-  `reason`      CHAR(255)         NOT NULL,
-  `points`      INT(255) UNSIGNED NOT NULL,
-  `date`        TIMESTAMP         NOT NULL DEFAULT (sysdate() + interval 2 hour),
-  `expire_date` TIMESTAMP         NULL     DEFAULT NULL,
-  `moderator`   CHAR(255)         NOT NULL,
-  `type`        CHAR(255)         NOT NULL,
-  `state`       CHAR(255)         NOT NULL DEFAULT 'active'
+  `id`                   INT(255) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `player_uuid`          CHAR(255)         NOT NULL,
+  `sanction_name`        CHAR(255)         NOT NULL,
+  `sanction_reason`      CHAR(255)         NOT NULL,
+  `sanction_points`      INT(255) UNSIGNED NOT NULL,
+  `sanction_beginning_date`  TIMESTAMP         NOT NULL DEFAULT (sysdate() + interval 2 hour),
+  `sanction_ending_date`    TIMESTAMP         NULL     DEFAULT NULL,
+  `sanction_author_uuid` CHAR(255)         NOT NULL,
+  `sanction_type`        CHAR(255)         NOT NULL,
+  `sanction_state`       CHAR(255)         NOT NULL DEFAULT 'active'
 );
