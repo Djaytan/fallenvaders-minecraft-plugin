467dfa5255502e12608a2f6a29c872d298fb23c0
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-10-25 17:34:14 +0200

    feat: FV-104 - add sanction state enum

---
+++ libraries/justice-hands/src/main/java/fr/fallenvaders/minecraft/justice_hands/SanctionState.java
@@ -0,0 +1,31 @@
+/*
+ *  This file is part of the FallenVaders distribution (https://github.com/FallenVaders).
+ *  Copyright © 2021 Loïc DUBOIS-TERMOZ.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fr.fallenvaders.minecraft.justice_hands;
+
+/**
+ * This enum represents the state of a sanction.
+ *
+ * @author FallenVaders' dev team
+ * @since 0.3.0
+ */
+public enum SanctionState {
+  /** The sanction is currently active. */
+  ACTIVE,
+  /** The sanction is now expired and is not applied on the server. */
+  EXPIRED
+}
