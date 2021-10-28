48e547067950c2fa44b25fffee62cb9a37e4585f
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-10-28 11:46:01 +0200

    refactor: FV-104 - update & factorise code

--- libraries/justice-hands/src/main/java/fr/fallenvaders/minecraft/justicehands/view/KeysKeeperComponent.java
+++ libraries/justice-hands/src/main/java/fr/fallenvaders/minecraft/justicehands/view/KeysKeeperComponent.java
@@ -18,16 +18,27 @@
 package fr.fallenvaders.minecraft.justicehands.view;
 
 import fr.fallenvaders.minecraft.justicehands.GeneralUtils;
+import fr.fallenvaders.minecraft.justicehands.JusticeHandsException;
+import fr.fallenvaders.minecraft.justicehands.model.entities.JhSanction;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class KeysKeeperComponent {
+
   public static final String DAYTIME_PATTERN = "dd/MM/yyyy HH:mm:ss";
   public static final SimpleDateFormat SDF =
       new SimpleDateFormat(DAYTIME_PATTERN, new Locale("fr", "FR"));
+
+  public static final String LOGIN_BAN_DEF_MESSAGE =
+      "%s\n§cVous avez été banni définitivement du serveur pour la raison suivante : \n\n§bID de Sanction : §7%s§b - §7%s\n§bRaison : §7%s\n§bDate : §7%s";
+  public static final String LOGIN_BAN_MESSAGE =
+      "%s\n§cVous avez été banni temporairement du serveur pour la raison suivante : \n\n§bID de Sanction : §7%s§b - §7%s\n§bRaison : §7%s\n§bDate : §7%s §b- §7%s\n§bTemps restant : §7%s";
 
   public static Component ejectingMessageCpnt(CJSanction sanction) {
     String date = SDF.format(sanction.getTSDate().getTime());
@@ -46,39 +57,32 @@ public class KeysKeeperComponent {
     return cpnt;
   }
 
-  public static Component loginBanMessage(CJSanction sanction) {
-    String date = SDF.format(sanction.getTSDate().getTime());
-    String expireDate = SDF.format(sanction.getTSExpireDate().getTime());
+  public static Component loginBanMessage(JhSanction jhSanction, boolean isBanDef)
+      throws JusticeHandsException {
+    List<String> data = new ArrayList<>();
+    data.add(GeneralUtils.getPrefix("kk")); // prefix
+    data.add(Integer.toString(jhSanction.getSctnId())); // sanction ID
+    data.add(jhSanction.getSctnName()); // sanction name
+    data.add(jhSanction.getSctnReason()); // sanction reason
+    data.add(SDF.format(jhSanction.getSctnBeginningDate().getTime())); // sanction beginning date
 
-    final Component cpnt =
-        LegacyComponentSerializer.legacyAmpersand()
-            .deserialize(
-                String.format(
-                    "%s\n§cVous avez été banni temporairement du serveur pour la raison suivante : \n\n§bID de Sanction : §7%s§b - §7%s\n§bRaison : §7%s\n§bDate : §7%s §b- §7%s\n§bTemps restant : §7%s",
-                    GeneralUtils.getPrefix("kk"),
-                    sanction.getID(),
-                    sanction.getName(),
-                    sanction.getReason(),
-                    date,
-                    expireDate,
-                    GeneralUtils.timeRemaining(
-                        sanction.getTSExpireDate().getTime() - System.currentTimeMillis())));
-    return cpnt;
-  }
+    String banMessage;
+    if (isBanDef) {
+      banMessage = LOGIN_BAN_DEF_MESSAGE;
+    } else {
+      if (jhSanction.getSctnEndingDate() != null) {
+        data.add(SDF.format(jhSanction.getSctnEndingDate().getTime())); // sanction ending date
+        data.add(
+            GeneralUtils.timeRemaining(
+                jhSanction.getSctnEndingDate().getTime()
+                    - System.currentTimeMillis())); // sanction remaining time
+        banMessage = LOGIN_BAN_MESSAGE;
+      } else {
+        throw new JusticeHandsException(
+            "The ending date for a non-definitive ban mustn't be null.");
+      }
+    }
 
-  public static Component loginBanDefMessage(CJSanction sanction) {
-    String date = SDF.format(sanction.getTSDate().getTime());
-
-    final Component cpnt =
-        LegacyComponentSerializer.legacyAmpersand()
-            .deserialize(
-                String.format(
-                    "%s\n§cVous avez été banni définitivement du serveur pour la raison suivante : \n\n§bID de Sanction : §7%s§b - §7%s\n§bRaison : §7%s\n§bDate : §7%s",
-                    GeneralUtils.getPrefix("kk"),
-                    sanction.getID(),
-                    sanction.getName(),
-                    sanction.getReason(),
-                    date));
-    return cpnt;
+    return LegacyComponentSerializer.legacyAmpersand().deserialize(String.format(banMessage, data));
   }
 }
