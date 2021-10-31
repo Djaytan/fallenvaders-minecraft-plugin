7fcff01ba6268425f50f5899b4aa50b552ce314b
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-10-31 18:21:50 +0100

    feat: FV-104 - make an implementation of CriticalError interface

---
+++ libraries/commons/src/main/java/fr/fallenvaders/minecraft/commons/CriticalPluginError.java
@@ -0,0 +1,70 @@
+/*
+ *  This file is part of the FallenVaders distribution (https://github.com/FallenVaders).
+ *  Copyright © 2021 Loïc DUBOIS-TERMOZ.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package fr.fallenvaders.minecraft.commons;
+
+import javax.inject.Inject;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+
+/**
+ * This is an implementation class {@link CriticalError} in case of FallenVaders' plugin.
+ *
+ * @author FallenVaders' dev team
+ * @since 0.3.0
+ */
+public class CriticalPluginError implements CriticalError {
+
+  private final JavaPlugin plugin;
+  private final Logger logger;
+  private final PluginManager pluginManager;
+
+  /**
+   * Constructor.
+   *
+   * @param plugin The Bukkit Java plugin.
+   * @param logger The logger.
+   * @param pluginManager The Bukkit plugin manager.
+   */
+  @Inject
+  public CriticalPluginError(
+      @NotNull JavaPlugin plugin, @NotNull Logger logger, @NotNull PluginManager pluginManager) {
+    this.plugin = plugin;
+    this.logger = logger;
+    this.pluginManager = pluginManager;
+  }
+
+  @Override
+  public void raiseError(@Nullable String message) {
+    raiseError(message, null);
+  }
+
+  @Override
+  public void raiseError(@Nullable String message, @Nullable Throwable cause) {
+    if (message != null) {
+      if (cause != null) {
+        logger.error(message, cause);
+      } else {
+        logger.error(message);
+      }
+    }
+    pluginManager.disablePlugin(plugin);
+  }
+}
