0296cfdc3fe3e399e758e8a9cc755449a638ceee
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-11-03 13:56:43 +0100

    refactor: FV-104 - remove no longer used classes

--- libraries/justice-hands/src/main/java/fr/fallenvaders/minecraft/justicehands/JusticeHandsModule.java
+++ libraries/justice-hands/src/main/java/fr/fallenvaders/minecraft/justicehands/JusticeHandsModule.java
@@ -2,10 +2,8 @@ package fr.fallenvaders.minecraft.justicehands;
 
 import co.aikar.commands.PaperCommandManager;
 import fr.fallenvaders.minecraft.commons.FvModule;
-import fr.fallenvaders.minecraft.justicehands.view.viewmodel.CategoriesList;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -20,7 +18,6 @@ public final class JusticeHandsModule extends FvModule {
   /** This is the module's name. */
   public static final String MODULE_NAME = "justice-hands";
 
-  private final FileConfiguration config;
   private final PaperCommandManager paperCommandManager;
 
   private final CommandsInitializer commandsInitializer;
@@ -29,19 +26,16 @@ public final class JusticeHandsModule extends FvModule {
   /**
    * Constructor.
    *
-   * @param config The Bukkit plugin config file instance.
    * @param paperCommandManager The Paper command manager of aïkar lib.
    * @param commandsInitializer The initializer of Bukkit commands.
    * @param listenersInitializer The initializer of Bukkit listeners.
    */
   @Inject
   public JusticeHandsModule(
-      @NotNull FileConfiguration config,
       @NotNull PaperCommandManager paperCommandManager,
       @NotNull CommandsInitializer commandsInitializer,
       @NotNull ListenersInitializer listenersInitializer) {
     super(MODULE_NAME);
-    this.config = config;
     this.paperCommandManager = paperCommandManager;
     this.commandsInitializer = commandsInitializer;
     this.listenersInitializer = listenersInitializer;
@@ -51,8 +45,5 @@ public final class JusticeHandsModule extends FvModule {
   public void onEnable() {
     commandsInitializer.initialize();
     listenersInitializer.initialize();
-
-    // Load sanctions from config folder
-    CategoriesList.getSanctionsConfig(config);
   }
 }
