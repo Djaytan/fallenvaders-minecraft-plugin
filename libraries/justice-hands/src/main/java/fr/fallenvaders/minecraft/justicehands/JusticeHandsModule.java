0ba4ce0bb81cd4c80399abcd69a656dd6e7618ae
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-11-18 01:58:35 +0100

    feat: FV-104 - initialize database of JusticeHands

--- libraries/justice-hands/src/main/java/fr/fallenvaders/minecraft/justicehands/JusticeHandsModule.java
+++ libraries/justice-hands/src/main/java/fr/fallenvaders/minecraft/justicehands/JusticeHandsModule.java
@@ -1,9 +1,14 @@
 package fr.fallenvaders.minecraft.justicehands;
 
+import fr.fallenvaders.minecraft.commons.CriticalErrorRaiser;
 import fr.fallenvaders.minecraft.commons.FvModule;
+import fr.fallenvaders.minecraft.commons.sql.DatabaseInitializer;
+import org.jetbrains.annotations.NotNull;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import org.jetbrains.annotations.NotNull;
+import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * Entry point class for JusticeHands module.
@@ -18,26 +23,42 @@ public final class JusticeHandsModule extends FvModule {
   /** This is the module's name. */
   public static final String MODULE_NAME = "justice-hands";
 
+  private static final String SQL_INITIALIZE_SCRIPT = "initialize.sql";
+
   private final CommandsInitializer commandsInitializer;
+  private final CriticalErrorRaiser criticalErrorRaiser;
+  private final DatabaseInitializer databaseInitializer;
   private final ListenersInitializer listenersInitializer;
 
   /**
    * Constructor.
    *
    * @param commandsInitializer The {@link CommandsInitializer}.
+   * @param criticalErrorRaiser The {@link CriticalErrorRaiser}.
+   * @param databaseInitializer The {@link DatabaseInitializer}.
    * @param listenersInitializer The {@link ListenersInitializer}.
    */
   @Inject
   public JusticeHandsModule(
       @NotNull CommandsInitializer commandsInitializer,
+      @NotNull CriticalErrorRaiser criticalErrorRaiser,
+      @NotNull DatabaseInitializer databaseInitializer,
       @NotNull ListenersInitializer listenersInitializer) {
     super(MODULE_NAME);
     this.commandsInitializer = commandsInitializer;
+    this.criticalErrorRaiser = criticalErrorRaiser;
+    this.databaseInitializer = databaseInitializer;
     this.listenersInitializer = listenersInitializer;
   }
 
   @Override
   public void onEnable() {
+    try {
+      databaseInitializer.initialize(this.getClass(), SQL_INITIALIZE_SCRIPT);
+    } catch (IOException | SQLException e) {
+      criticalErrorRaiser.raise(
+          "Fail to execute SQL initialize script. Raise a critical error to prevent bad effects.");
+    }
     commandsInitializer.initialize();
     listenersInitializer.initialize();
   }
