d41a437604a19d78de276bfdc3944a8f6264f147
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-11-21 07:27:01 +0100

    setup Spring boot framework

---
+++ src/main/kotlin/fr/fallenvaders/minecraft/mcfallenvaders/McFallenVadersApplication.kt
@@ -0,0 +1,11 @@
+package fr.fallenvaders.minecraft.mcfallenvaders
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class McFallenVadersApplication
+
+fun main(args: Array<String>) {
+	runApplication<McFallenVadersApplication>(*args)
+}
---
+++ src/test/kotlin/fr/fallenvaders/minecraft/mcfallenvaders/McFallenVadersApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.fallenvaders.minecraft.mcfallenvaders
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class McFallenVadersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
