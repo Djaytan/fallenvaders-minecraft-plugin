de222c3e9f3bf934dc7c0da272f1b5b775669049
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-11-27 23:10:32 +0100

    restart from scratch: setup mod environment and set version to 0.1.0

--- src/main/kotlin/fr/fallenvaders/minecraft/plugin/SanctionRepository.kt
+++
@@ -1,5 +0,0 @@
-package fr.fallenvaders.minecraft.plugin
-
-import org.springframework.data.repository.CrudRepository
-
-interface SanctionRepository : CrudRepository<Sanction, Long>
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO sanction (name, description)
-VALUES ('Sanction #1', 'Description'),
-       ('Sanction #2', 'Description')
