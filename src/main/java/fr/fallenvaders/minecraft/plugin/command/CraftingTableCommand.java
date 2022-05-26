9169b8a38f71e38033b2445f7ee58f22229aca8b
Voltariuss <loic.duboistermoz@gmail.com>
2022-05-26 20:24:19 +0200

    refactor(essentials): remove /workbench alias

--- src/main/java/fr/fallenvaders/minecraft/plugin/command/CraftingTableCommand.java
+++ src/main/java/fr/fallenvaders/minecraft/plugin/command/CraftingTableCommand.java
@@ -12,7 +12,7 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 @Singleton
-@CommandAlias("craftingtable|workbench|craft")
+@CommandAlias("craftingtable|craft")
 @CommandPermission("fallenvaders.essentials.craftingtable")
 public class CraftingTableCommand extends BaseCommand {
 
